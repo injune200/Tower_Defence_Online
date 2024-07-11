d5b5e407b5639ea4091a5dd8a9d4f56e052faefe
wjdrbsgkrry <wjdrbsgkrry@naver.com>
2024-07-11 20:15:10 +0900

    try catch문 적용

--- src/handlers/account/login.handler.js
+++ src/handlers/account/login.handler.js
@@ -9,7 +9,7 @@ const router = express.Router();
 const loginHandler = router.post("/login", async (req, res, io) => {
   const { username, password } = req.body;
 
-  {
+  try {
     const client = createClient();
     client.on("error", (err) => console.log("Redis Client Error", err));
     await client.connect();
@@ -40,6 +40,9 @@ const loginHandler = router.post("/login", async (req, res, io) => {
     res.setHeader("authorization", `Bearer ${token}`);
 
     return res.status(200).json({ message: "로그인 성공", data: username });
+  } catch (error) {
+    console.error("로그인에 오류 발생!", error);
+    return res.status(500).json("Server Error: 500");
   }
 });
 
