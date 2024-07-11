c44a23cbee8434af848d03ed5d596340870d07a0
wjdrbsgkrry <wjdrbsgkrry@naver.com>
2024-07-11 21:03:24 +0900

    redis 주소 입력

--- src/handlers/account/login.handler.js
+++ src/handlers/account/login.handler.js
@@ -1,17 +1,24 @@
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import { createClient } from "redis";
-import express from "express";
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import { createClient } from 'redis';
+import express from 'express';
 
 // / 로그인 API /
 const router = express.Router();
 
-const loginHandler = router.post("/login", async (req, res, io) => {
+const loginHandler = router.post('/login', async (req, res, io) => {
   const { username, password } = req.body;
 
   try {
-    const client = createClient();
-    client.on("error", (err) => console.log("Redis Client Error", err));
+    const client = createClient({
+      username: 'default', // use your Redis user. More info https://redis.io/docs/latest/operate/oss_and_stack/management/security/acl/
+      password: '1234', // use your password here
+      socket: {
+        host: '3.22.236.177',
+        port: 6379,
+      },
+    });
+    client.on('error', (err) => console.log('Redis Client Error', err));
     await client.connect();
 
     await client.set(username, password); // 테스트용 코드 (key : value 형식)
@@ -19,30 +26,29 @@ const loginHandler = router.post("/login", async (req, res, io) => {
     const user = await client.get(username);
     const saltRounds = await bcrypt.genSalt(10); // 테스트용 코드
     const hashedPassword = await bcrypt.hash(password, saltRounds); // 테스트용 코드
-    if (!user)
-      return res.status(401).json({ message: "존재하지 않는 계정입니다." });
+    if (!user) return res.status(401).json({ message: '존재하지 않는 계정입니다.' });
     else if (!(await bcrypt.compare(password, hashedPassword)))
-      return res.status(401).json({ message: "비밀번호가 일치하지 않습니다." });
+      return res.status(401).json({ message: '비밀번호가 일치하지 않습니다.' });
     // bcrypt.compare 변경해야함 테스트용 코드
 
     const token = jwt.sign(
       {
-        type: "JWT",
+        type: 'JWT',
         user_id: user.id,
       },
-      "10",
+      '10',
       {
-        expiresIn: "60m",
+        expiresIn: '60m',
       },
     );
 
-    console.log("로그인 성공\nuserData:", user);
-    res.setHeader("authorization", `Bearer ${token}`);
+    console.log('로그인 성공\nuserData:', user);
+    res.setHeader('authorization', `Bearer ${token}`);
 
-    return res.status(200).json({ message: "로그인 성공", data: username });
+    return res.status(200).json({ message: '로그인 성공', data: username });
   } catch (error) {
-    console.error("로그인에 오류 발생!", error);
-    return res.status(500).json("Server Error: 500");
+    console.error('로그인에 오류 발생!', error);
+    return res.status(500).json('Server Error: 500');
   }
 });
 
