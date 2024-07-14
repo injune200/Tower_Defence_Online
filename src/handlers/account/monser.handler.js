3cdfd5de9169c315a4d32b31fe63e70f5d052d56
wjdrbsgkrry <wjdrbsgkrry@naver.com>
2024-07-14 17:35:56 +0900

    try catch문 적용

--- src/handlers/account/monser.handler.js
+++ src/handlers/account/monser.handler.js
@@ -18,9 +18,14 @@ export const responseMonster = (socket, payload) => {
   user.monsters.push(payload.monsterData); //서버 데이터 업데이트
 
   //상대방에게도 알리는 전달 코드
-  const gameSession = getGame(user.gameId);
-  const opponentUser = gameSession.getOpponentUser(uuid);
-  opponentUser.socket.emit('createOpponent', { payload: payload.monsterData });
+  try {
+    const gameSession = getGame(user.gameId);
+    const opponentUser = gameSession.getOpponentUser(uuid);
+    opponentUser.socket.emit('createOpponent', { payload: payload.monsterData });
+  } catch (err) {
+    console.log(err);
+    return { status: 'fail', message: `서버 내의 오류 발생` };
+  }
 
   return { status: 'success', message: '몬스터 생성 완료' };
 };
@@ -33,6 +38,7 @@ export const removeMonser = (socket, payload) => {
   if (!user)
     return { status: 'fail', message: '존재하지 않는 유저 또는 유효하지 않은 요청입니다.' };
 
+  const isExistMonster = null;
   for (let monster of user.monsters) {
     if (
       monsterNumber == monster.monsterNumber &&
@@ -40,7 +46,7 @@ export const removeMonser = (socket, payload) => {
       level == monster.level &&
       creationTime == monster.creationTime
     ) {
-      return;
+      isExistMonster = true;
     }
   }
 
@@ -48,9 +54,13 @@ export const removeMonser = (socket, payload) => {
     for (let i = 0; i < user.monsters.length; i++) {
       if (user.monsters[i] == isExistMonster) {
         user.monsters.splice(i, 1);
-
-        const opponentUser = gameSession.getOpponentUser(uuid);
-        opponentUser.socket.emit('removeOpponent', { payload: payload.monsterData });
+        try {
+          const opponentUser = gameSession.getOpponentUser(uuid);
+          opponentUser.socket.emit('removeOpponent', { payload: payload.monsterData });
+        } catch (err) {
+          console.log(err);
+          return { status: 'fail', message: `서버 내의 오류 발생` };
+        }
 
         return { status: 'success', message: '몬스터 삭제 완료' };
       }
